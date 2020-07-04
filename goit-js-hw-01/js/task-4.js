b1261e247cb75667567fd2a86094eb990c401a0a
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-07-05 00:21:55 +0300

    some fix

--- goit-js-hw-01/js/task-4.js
+++ goit-js-hw-01/js/task-4.js
@@ -2,31 +2,18 @@ const credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 let droidAmount;
-let message;
 
 droidAmount = prompt('Какое количество дроидов желаете купить?');
 
-if (droidAmount === null) {
-    message = 'Отменено пользователем';
-    alert (message);
-} else {
-    droidAmount = Number(Math.round(droidAmount));
-}
+droidAmount === null ? console.log ('Отменено пользователем') : droidAmount = Number(Math.round(droidAmount));
 
-if (droidAmount <= 0) {
-    message = 'Введите число больше 0';
-    alert (message);
-} else {
-    totalPrice = droidAmount * pricePerDroid;
-}
+droidAmount <= 0 ? console.log ('Введите число больше 0') : totalPrice = droidAmount * pricePerDroid;
 
 if (totalPrice > credits) {
-    message = 'Недостаточно средств на счету!';
-    alert (message);
+    console.log ('Недостаточно средств на счету!');
 } else {
     const creditsBalance = credits - totalPrice;
-    message = `Вы купили ${droidAmount} дроидов, на счету осталось ${creditsBalance} кредитов.`;
-    console.log (message);
+    console.log (`Вы купили ${droidAmount} дроидов, на счету осталось ${creditsBalance} кредитов.`);
 }
 
 
