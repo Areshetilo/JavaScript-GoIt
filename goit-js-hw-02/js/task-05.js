396a85c292f09afdb378fab5a090f8584b805662
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-07-12 12:43:56 +0300

    added task05 HW2

---
+++ goit-js-hw-02/js/task-05.js
@@ -0,0 +1,20 @@
+const checkForSpam = function(message) {
+    let messageArray = message.toLowerCase();
+
+        if (messageArray.includes('spam') || messageArray.includes('sale')) {
+            return true;
+        }
+        return false;
+
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
