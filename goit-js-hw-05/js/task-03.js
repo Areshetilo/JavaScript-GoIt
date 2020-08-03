55aa8e250fb521535efbcea59f49145a7fb058ea
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-08-03 23:27:15 +0300

    added task03 HW5

---
+++ goit-js-hw-05/js/task-03.js
@@ -0,0 +1,30 @@
+class Storage {
+    constructor(arr) {
+        this._items = arr;
+    }
+    get items(){
+        return this._items;
+    }
+    addItem(name){
+        this._items.includes(name) ? console.log(`Товар '${name}' уже есть`): this._items.push(name);
+    }
+    removeItem(name){
+        this._items = this._items.filter(item => item !== name);
+    }
+}
+
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+]);
+
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
