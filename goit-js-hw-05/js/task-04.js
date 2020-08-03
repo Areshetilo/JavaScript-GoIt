6d91a707d6fac4a530d7935b51906b2f2e718bb2
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-08-03 23:48:43 +0300

    task4 HW5 fix

--- goit-js-hw-05/js/task-04.js
+++ goit-js-hw-05/js/task-04.js
@@ -7,12 +7,15 @@ class StringBuilder {
     }
     append(str) {
         return (this._value += str);
+        // this._value = `${this._value}${str}`; 2 вариант
     }
     prepend(str) {
         return (this._value = str + this._value);
+        // this._value = `${str}${this._value}`; 2 вариант
     }
     pad(str) {
         return (this._value = str + this.value + str);
+        // this._value = `${str}${this._value}${str}`; 2 вариант
     }
 }
 
