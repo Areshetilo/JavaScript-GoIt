bab1e191d4751a5d64fda9327869da768c9b985f
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-09-14 18:36:04 +0300

    added last update

--- goit-js-hw-08-gallery/js/main.js
+++ goit-js-hw-08-gallery/js/main.js
@@ -5,18 +5,16 @@ const refs = {
     originalImg: document.querySelector('.lightbox__image'),
     modal: document.querySelector('.js-lightbox'),
     closeModal: document.querySelector('[data-action="close-lightbox"]'),
-    overlay: document.queryCommandEnabled('.lightbox__content'),
+    overlay: document.querySelector('.lightbox__content'),
 }
-
+let currentIndex = 0;
 createGallery();
 refs.gallery.addEventListener('click', onClickGalleryImg);
-refs.closeModal.addEventListener('click', closeModal);
-document.addEventListener('keydown', onKeyPress);
 
 function createGallery() {
     let index = 0;
     const galleryUlTags = galleryItems.map ((galleryItems) => {
-        index += 1;
+
         const li = document.createElement("li");
         const a = document.createElement("a");
         const img = document.createElement("img");
@@ -33,6 +31,7 @@ function createGallery() {
 
         a.appendChild(img);
         li.appendChild(a);
+        index += 1;
 
         return li
     })
@@ -47,34 +46,65 @@ function onClickGalleryImg(event) {
     openModal();
     let originalImgURL = event.target.dataset.source;
     let originalImgALT = event.target.alt;
-
+    currentIndex = +event.target.dataset.index;
+    console.log(currentIndex, "cjplfkn")
     setOriginalImgUrl(originalImgURL);
     setOriginalImgAlt(originalImgALT);
-
+    setOriginalImgIndex(currentIndex);
+    refs.closeModal.addEventListener('click', closeModal);
+    refs.overlay.addEventListener('click', closeOverlay);
+    window.addEventListener('keydown', onKeyPress);
 }
 
 function openModal() {
-    refs.modal.classList.add("is-open");
+    refs.modal.classList.toggle("is-open");
 }
 
 function closeModal() {
-    refs.modal.classList.remove("is-open");
+    refs.modal.classList.toggle("is-open");
     deleteOriginalImgUrl();
+    refs.closeModal.removeEventListener('click', closeModal);
+    refs.overlay.removeEventListener('click', closeOverlay);
+    window.removeEventListener('keydown', onKeyPress);
+}
+
+function closeOverlay(event) {
+    if(event.target.className === 'lightbox__content') {
+        refs.modal.classList.toggle("is-open");
+        deleteOriginalImgUrl();
+    }
 }
 
 function onKeyPress(event) {
-
     if(event.code === "Escape") {
         closeModal();
+    } else if(event.code === "ArrowLeft") {
+        leftKeyPress();
+    } else if(event.code === "ArrowRight") {
+        rightKeyPress();
     }
 }
 
 function rightKeyPress() {
-
+    if((currentIndex + 1) > galleryItems.length - 1) {
+        currentIndex = 0;
+        refs.originalImg.src = galleryItems[currentIndex].original;
+    } else {
+    currentIndex += 1;
+    refs.originalImg.src = galleryItems[currentIndex].original;
+    }
+    console.log(currentIndex);
 }
 
 function leftKeyPress() {
-
+    if(currentIndex === 0) {
+        currentIndex = galleryItems.length - 1;
+        refs.originalImg.src = galleryItems[currentIndex].original;
+    } else {
+        currentIndex -= 1;
+        refs.originalImg.src = galleryItems[currentIndex].original;
+    }
+    console.log(currentIndex);
 }
 
 function setOriginalImgUrl(url) {
@@ -88,5 +118,9 @@ function deleteOriginalImgUrl() {
 
 function setOriginalImgAlt(alt) {
     refs.originalImg.setAttribute("alt", alt)
+}
+
+function setOriginalImgIndex(index) {
+    refs.originalImg.setAttribute("data-index", index)
 }
 
